b0df53e3459234ab681c6e514403bbf6df36b558
Chernyh <blackvano@rambler.ru>
2022-09-01 22:05:52 +0500

    test5

--- seminar1/test5/Program.cs
+++ seminar1/test5/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Вывести последнюю цифру трехзначного числа
+Console.Write("Введите целое трехзначное число ");
+try
+    {
+    int n = Convert.ToInt32(Console.ReadLine()); // Convert.ToInt32 переводит из строки в целое число
+    int b = n % 10;
+    Console.WriteLine("Последняя цифра числа " +b);
+    }
+catch
+{
+    Console.WriteLine("Надо вводить именно целое число");
+}
