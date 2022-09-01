6d3170820ec47c6746b0732a496c8e53c1b7c50c
Chernyh <blackvano@rambler.ru>
2022-09-01 21:44:50 +0500

    test4

---
+++ seminar1/test4/Program.cs
@@ -0,0 +1,18 @@
+﻿// От введенного числа N показываются все целые числа от -N до +N
+Console.Write("Введите целое число ");
+try
+    {
+    int n = Convert.ToInt32(Console.ReadLine()); // Convert.ToInt32 переводит из строки в целое число
+    int b = -n;
+    Console.WriteLine("Диапазон от " +b);
+    Console.WriteLine("Диапазон до " +n);
+        while (b<=n)
+        {
+           Console.Write(b+",");
+           b++;
+        }
+    }
+catch
+{
+    Console.WriteLine("надо вводить именно целое число");
+}
