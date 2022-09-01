40920f2bcb2408f68dc214e3d03c05effc6f498c
Chernyh <blackvano@rambler.ru>
2022-09-01 23:28:35 +0500

    DZ1

---
+++ seminar1/DZ1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Нахождение максимального числа из двух
+try 
+    {
+    Console.Write("Введите целое число a ");
+    int a = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите целое число b ");
+    int b = Convert.ToInt32(Console.ReadLine());
+    if (a>b) Console.Write("Первое число максимальное ="+a);
+    else Console.Write("Второе число максимальное ="+b);
+    }
+catch
+{
+    Console.Write("Введите номер а не текст");
+}
