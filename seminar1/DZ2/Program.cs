ac5b55ab15af7c392b3e972330d74a65626f9f11
Chernyh <blackvano@rambler.ru>
2022-09-01 23:56:09 +0500

    DZ2

---
+++ seminar1/DZ2/Program.cs
@@ -0,0 +1,26 @@
+﻿/// Нахождение максимального числа из трех
+try 
+    {
+    Console.Write("Введите целое число a ");
+    int a = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите целое число b ");
+    int b = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите целое число с ");
+    int c = Convert.ToInt32(Console.ReadLine());
+    if(a>b)
+    {
+    if(a>c)Console.Write("Первое число максимальное ="+a);
+    }
+    if(b>a)
+    {
+    if(b>c)Console.Write("Второе число максимальное ="+b);
+    }
+    if(c>a)
+    {
+    if(c>b)Console.Write("Третье число максимальное ="+c);
+    }
+    }
+catch
+{
+    Console.Write("Введите число а не текст");
+}
