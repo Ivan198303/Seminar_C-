a4bf7dcff6657d93cd1358ff46f5746a94c1487f
Chernyh <blackvano@rambler.ru>
2022-09-02 00:23:44 +0500

    DZ4

---
+++ seminar1/DZ4/Program.cs
@@ -0,0 +1,16 @@
+﻿// Проверить число на четность и выыести все четные числа
+Console.Write("Введите целое число ");
+try
+    {
+    int a = Convert.ToInt32(Console.ReadLine()); 
+    int b =2;
+         while (b<=a)
+        {
+           Console.Write(b+",");
+           b=b+2;
+        }
+    }
+catch
+{
+    Console.WriteLine("Надо вводить именно целое число");
+}
