c06aca153ed5841c42885b02913d6ec19f336c0b
Chernyh <blackvano@rambler.ru>
2022-09-02 00:13:51 +0500

    DZ3

---
+++ seminar1/DZ3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Проверить число на четность
+Console.Write("Введите целое число ");
+try
+    {
+    int a = Convert.ToInt32(Console.ReadLine()); // Convert.ToInt32 переводит из строки в целое число
+        if (a%2==0)
+        {
+        Console.WriteLine("Число четное");
+        } 
+        else Console.WriteLine("Число нечетное ");
+    }
+catch
+{
+    Console.WriteLine("Надо вводить именно целое число");
+}
