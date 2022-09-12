3a3f6c2151e0cf3983d2c9603243c34d9c5ebd0d
Chernyh <blackvano@rambler.ru>
2022-09-12 10:43:47 +0500

    seminar3 ZD3

--- seminar3/ZD3/Program.cs
+++ seminar3/ZD3/Program.cs
@@ -1,3 +1,26 @@
 ﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 //3 -> 1, 8, 27
 //5 -> 1, 8, 27, 64, 125
+void HomeTask() 
+{
+Console.WriteLine("Семинар 3. ДЗ №3"); 
+}
+HomeTask(); 
+try
+{
+    Console.Write("Введите целое число ");
+    int a = Convert.ToInt32(Console.ReadLine()); 
+    void cube(int a)
+    {
+        for(int b=1; b<a; b++)
+        {
+            Console.Write($"{b*b*b},");
+        }
+    } 
+    cube(a);       
+}
+catch
+{
+    
+    Console.WriteLine("Надо вводить именно целое число");
+}
