bf4cc9e073d974a8b39e5baff98c7cebb8ea9bd8
Chernyh <blackvano@rambler.ru>
2022-09-21 14:35:30 +0500

    seminar6 zd

--- seminar6/ZD1/Program.cs
+++ seminar6/ZD1/Program.cs
@@ -1,4 +1,32 @@
 ﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
 // 0, 7, 8, -2, -2 -> 2
 // -7, 567, 89, 223-> 3
-Console.WriteLine("Hello, World!");
+int[] MassivNumber(int num) // Создаем массив и заполняем введеными числами
+{
+    int[] array = new int[num];
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Введите число №{i + 1}: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+int Amount(int[] num) // считаем кол-во чисел больше 0.
+{
+    int count = 0;
+    for (int i = 0; i < num.Length; i++)
+    {
+        if (num[i] > 0) count++;
+    }
+    return count;
+}
+try
+{
+    Console.Write("Введите необходимое количество чисел: ");
+    int a = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"Количество введенных чисел больше 0 = {(string.Join(",", Amount(MassivNumber(a))))}");
+}
+catch
+{
+    Console.WriteLine("Надо вводить число а не текст");
+}
