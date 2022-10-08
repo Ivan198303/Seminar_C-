40ee32fc663d78aa332505f8df4b1284d2ff9d4a
Chernyh Ivan <blackvano@rambler.ru>
2022-10-08 16:43:51 +0500

    seminar8 ZD4

--- seminar8/ZD4/Program.cs
+++ seminar8/ZD4/Program.cs
@@ -5,3 +5,49 @@
 // 34(1,0,0) 41(1,1,0)
 // 27(0,0,1) 90(0,1,1)
 // 26(1,0,1) 55(1,1,1)
+void FillArray(int[,,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            for (int K = 0; K < matr.GetLength(2); K++)
+            {
+                matr[i, j, K] = new Random().Next(10,100);
+            }
+        }
+    }
+}
+void PrintArray(int[,,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int K = 0; K < arr.GetLength(2); K++)
+            {
+                Console.Write($"{arr[i, j, K]} ({i},{j},{K})  ");
+            }
+             Console.WriteLine();
+        }
+    }
+}
+
+try
+{
+    System.Console.Write("Введите количество строк массива n = ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    System.Console.Write("Введите количество столбцов массива m = ");
+    int m = Convert.ToInt32(Console.ReadLine());
+    System.Console.Write("Введите количество слоев массива k = ");
+    int k = Convert.ToInt32(Console.ReadLine());
+    int[,,] matrix = new int[n, m, k];
+    FillArray(matrix);
+    Console.WriteLine();
+    PrintArray(matrix);
+    Console.WriteLine();
+}
+catch
+{
+    Console.WriteLine("Надо вводить число а не текст");
+}
